24d29f90a934ca287b4b50047294ff1601138d2c
Alexandre Rocha <juny14_15@hotmail.com>
2026-08-06 15:50:05 -0300

    Adicionado Commit Inicial

---
+++ tests/eAgenda.Testes.Unidade/Test1.cs
@@ -0,0 +1,16 @@
+﻿namespace eAgenda.Testes.Unidade;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        //A
+
+        //A
+
+        //A
+        
+    }
+}
