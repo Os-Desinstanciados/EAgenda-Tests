d40f926a5063eddec0496617384a502de37adf3c
vinicius.poli <alemaumvp@hotmail.com>
2026-08-10 13:57:42 -0300

    Adicionado Categoria Tests

---
+++ tests/eAgenda.Testes.Unidade/Modulos/ModuloCategoria/CategoriaTests.cs
@@ -0,0 +1,64 @@
+
+using eAgenda.Dominio.Modulos.ModuloCategoria;
+
+[TestClass]
+public sealed class CategoriaTestes
+{
+    [TestMethod]
+    public void Validar_ComNomeVazio_DeveRetornarErro()
+    {
+        //Arranjo [Configura dados do teste]
+        Categoria categoria = new Categoria(
+            string.Empty
+        );
+
+        //Ação [Executa a ação sob teste]
+        List<string> erros = categoria.Validar();
+
+        //Asserção [Checa o resultado comparado com o esperado]
+        Assert.HasCount(2, erros);
+        Assert.AreEqual(
+            "O campo \"Título\" deve ser preenchido.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_ComNomeCurto_DeveRetornarErro()
+    {
+        //Arranjo [Configura dados do teste]
+        Categoria categoria = new Categoria(
+            new string('A', 1)
+        );
+
+        //Ação [Executa a ação sob teste]
+        List<string> erros = categoria.Validar();
+
+        //Asserção [Checa o resultado comparado com o esperado]
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Título\" deve conter entre 2 e 100 caracteres.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_ComNomeLongo_DeveRetornarErro()
+    {
+        //Arranjo [Configura dados do teste]
+        Categoria categoria = new Categoria(
+            new string('A', 101)
+        );
+
+        //Ação [Executa a ação sob teste]
+        List<string> erros = categoria.Validar();
+
+        //Asserção [Checa o resultado comparado com o esperado]
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Nome\" deve conter entre 2 e 100 caracteres.",
+            erros.First()
+        );
+    }
+    
+}
