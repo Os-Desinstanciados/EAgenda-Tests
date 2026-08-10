c76e9f5133f2e8c290d52ea11701ec998da748e4
vinicius.poli <alemaumvp@hotmail.com>
2026-08-10 15:47:12 -0300

    Corrige DespesaTests

--- tests/eAgenda.Testes.Unidade/Modulos/ModuloDespesa/DespesaTests.cs
+++ tests/eAgenda.Testes.Unidade/Modulos/ModuloDespesa/DespesaTests.cs
@@ -29,7 +29,7 @@ public sealed class DespesaTestes
         List<string> erros = despesa.Validar();
 
         //Asserção [Checa o resultado comparado com o esperado]
-        Assert.HasCount(1, erros);
+        Assert.HasCount(2, erros);
         Assert.AreEqual(
             "O campo \"Descrição\" deve ser preenchido.",
             erros.First()
@@ -108,6 +108,81 @@ public sealed class DespesaTestes
         Assert.HasCount(1, erros);
         Assert.AreEqual(
             "O campo \"Data de Ocorrência\" deve ser preenchido.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_ComValorIgualMenorAZero_DeveRetornarErro()
+    {
+        //Arranjo [Configura dados do teste]
+        Categoria categoria = new Categoria("Limpeza");
+
+        Despesa despesa = new Despesa(
+            "Despesa",
+            new DateTime(2023, 03, 08),
+            0,
+            0,
+            new List<Categoria> { categoria }
+        );
+
+        //Ação [Executa a ação sob teste]
+        List<string> erros = despesa.Validar();
+
+        //Asserção [Checa o resultado comparado com o esperado]
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Valor\" deve ser maior que zero.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_ComFormaDePagamentoVazia_DeveRetornarErro()
+    {
+        //Arranjo [Configura dados do teste]
+        Categoria categoria = new Categoria("Limpeza");
+
+        Despesa despesa = new Despesa(
+            "Despesa",
+            new DateTime(2023, 03, 08),
+            100,
+            (FormaPagamento)999,
+            new List<Categoria> { categoria }
+        );
+
+        //Ação [Executa a ação sob teste]
+        List<string> erros = despesa.Validar();
+
+        //Asserção [Checa o resultado comparado com o esperado]
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Forma de Pagamento\" deve ser preenchido.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_ComNenhumaCategoria_DeveRetornarErro()
+    {
+        //Arranjo [Configura dados do teste]
+        
+
+        Despesa despesa = new Despesa(
+            "Despesa",
+            new DateTime(2023, 03, 08),
+            100,
+            (FormaPagamento)0, //Cast is redundant.
+            new List<Categoria> { }
+        );
+
+        //Ação [Executa a ação sob teste]
+        List<string> erros = despesa.Validar();
+
+        //Asserção [Checa o resultado comparado com o esperado]
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "Selecione ao menos uma categoria.",
             erros.First()
         );
     }    
